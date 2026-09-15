85c719c5f464403f2738e3ecf1cee5763a5132d3
FoxHimeZero <a3238297197@163.com>
2026-09-15 12:54:00 +0800

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,1 +1,10 @@
+#!/system/bin/sh
+ADB_DIR=/data/adb/all-id-spoof
+TMP_DIR=/data/local/tmp/widevine-spoof
 
+[ ! -f "$ADB_DIR/drm_id.txt" ] && exit 0
+
+mkdir -p "$TMP_DIR"
+chmod 755 "$TMP_DIR"
+cp -f "$ADB_DIR/drm_id.txt" "$TMP_DIR/custom_id"
+chmod 644 "$TMP_DIR/custom_id"
